699d6c3382a7d45be701b08b01ded90cf32d5ed0
Lukasz0104 <lukasz.01.stepien@gmail.com>
2022-02-08 23:21:15 +0100

    Added TODO. Added tests for every component

--- src/app/services/Randomizer/randomizer.service.spec.ts
+++ src/app/services/Randomizer/randomizer.service.spec.ts
@@ -1,19 +1,38 @@
 import { TestBed } from '@angular/core/testing';
+import { Choice } from 'src/app/choice';
 
 import { RandomizerService } from './randomizer.service';
 
 describe('RandomizerService', () =>
 {
 	let service: RandomizerService;
+	let choices: Array<Choice> = new Array<Choice>();
 
 	beforeEach(() =>
 	{
 		TestBed.configureTestingModule({});
 		service = TestBed.inject(RandomizerService);
+
+		for (let i = 0; i < 10; i++)
+		{
+			choices.push(new Choice(`choice-${i + 1}`));
+		}
 	});
 
 	it('should be created', () =>
 	{
 		expect(service).toBeTruthy();
+	});
+
+	it('should return one element when called with one parameter', () =>
+	{
+		let res = service.getRandom(choices)
+		expect(res).toHaveSize(1);
+	});
+
+	it('should return different subsets', () =>
+	{
+		let ret = service.getRandom(choices, 3);
+		expect(ret).not.toEqual(service.getRandom(choices, 3));
 	});
 });
